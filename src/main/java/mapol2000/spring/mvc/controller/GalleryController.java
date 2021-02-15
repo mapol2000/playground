03c6e8c643c7b9f351eb7526ca984467ba2047c2
mapol2000 <20909git>
2021-02-15 10:38:22 +0900

    팀원의 갤러리 기능 구현

---
+++ src/main/java/mapol2000/spring/mvc/controller/GalleryController.java
@@ -0,0 +1,14 @@
+package mapol2000.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GalleryController {
+
+    @GetMapping("gallery/list")
+    public String list() {
+        return "gallery/list";
+    }
+
+}
